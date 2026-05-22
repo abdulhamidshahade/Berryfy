063ac9952f1e089b134060fa922b66ead3143d8a
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-05-22 23:48:49 +0300

    Remove default values from properties in SendEmailRequest

--- Berryfy.Application/Dtos/EmailDtos/SendEmailRequest.cs
+++ Berryfy.Application/Dtos/EmailDtos/SendEmailRequest.cs
@@ -2,9 +2,9 @@
 {
     public class SendEmailRequest
     {
-        public string Recipient { get; set; } = string.Empty;
-        public string Subject { get; set; } = string.Empty;
-        public string Body { get; set; } = string.Empty;
-        public bool IsBodyHtml { get; set; } = true;
+        public string Recipient { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool IsBodyHtml { get; set; }
     }
 }
