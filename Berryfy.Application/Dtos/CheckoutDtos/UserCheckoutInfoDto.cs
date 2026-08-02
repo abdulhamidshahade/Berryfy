cdabb97350a877fe5cb967e518a7cfc3039bfa79
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-08-02 23:25:25 +0300

    Remove default value from LastName in UserCheckoutInfoDto
    Removed the automatic initialization of LastName to string.Empty in the UserCheckoutInfoDto class. LastName will now remain uninitialized unless explicitly set.

--- Berryfy.Application/Dtos/CheckoutDtos/UserCheckoutInfoDto.cs
+++ Berryfy.Application/Dtos/CheckoutDtos/UserCheckoutInfoDto.cs
@@ -7,7 +7,7 @@ namespace Berryfy.Application.Dtos.CheckoutDtos
         public string? SessionId { get; set; }
         
         public string FirstName { get; set; }
-        public string LastName { get; set; } = string.Empty;
+        public string LastName { get; set; }
         public string Email { get; set; } = string.Empty;
         public string? Phone { get; set; }
         public string Address { get; set; } = string.Empty;
