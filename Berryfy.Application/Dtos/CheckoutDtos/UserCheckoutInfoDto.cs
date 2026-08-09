dd31016c344836a147dad3f7d834d39fb4cb2977
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-08-09 23:58:23 +0300

    Remove default value from State property
    The State property no longer initializes to string.Empty by default. It is now a standard string property without an initial value assignment.

--- Berryfy.Application/Dtos/CheckoutDtos/UserCheckoutInfoDto.cs
+++ Berryfy.Application/Dtos/CheckoutDtos/UserCheckoutInfoDto.cs
@@ -13,7 +13,7 @@ namespace Berryfy.Application.Dtos.CheckoutDtos
         public string Address { get; set; }
         public string? Address2 { get; set; }
         public string City { get; set; }
-        public string State { get; set; } = string.Empty;
+        public string State { get; set; }
         public string ZipCode { get; set; } = string.Empty;
         public string Country { get; set; } = "US";
         
